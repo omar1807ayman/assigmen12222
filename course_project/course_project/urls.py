cb3023f994519539b6ade07dca354133e9daf248
oaji2 <o.a.j.i2002@gmail.com>
2024-06-08 20:11:09 +0300

    version 1

--- course_project/course_project/urls.py
+++ course_project/course_project/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # Includes Django auth URLs
 ]
